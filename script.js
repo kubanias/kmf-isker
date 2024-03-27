8b52d4c1252b5fd02ff8f7f5ca03ee4c6cd63f41
kubanias <bannyyi30@gmail.com>
2024-03-27 22:54:14 +0300

    Правки стилей и скриптов

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     navbar_menu();
     function navbar_menu() {
         $(".burger").click(function() {
-    
+            $(this).toggleClass('open');
             if($(".burger-menu").is(":visible")) {
                $(".burger-menu").removeClass("open");
             }
@@ -10,7 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
             else {
                 $(".burger-menu").addClass("open");
             }
-        
         });
     }
+    $('.burger-menu__link').click(function(e) {
+        e.preventDefault();
+        
+        var $list = $(this).find('.burger-menu__dropdown');
+        var $image = $(this).find('.vector');
+    
+        $list.toggle();
+        
+        // Переворачиваем изображение на 180 градусов
+        $image.toggleClass('rotate180');
+      });
 });
